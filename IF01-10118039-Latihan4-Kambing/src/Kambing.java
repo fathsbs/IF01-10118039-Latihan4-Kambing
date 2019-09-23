f0e3b29ec84f8edbb97fc3afdcc19f388c6b0701
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:03:11 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan4-Kambing/src/Kambing.java
@@ -0,0 +1,24 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class Kambing {
+
+    public void tambahKambing() {
+        //Deklarasi Variabel Lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing=jumlahKambing+5;
+        System.out.println("Jumlah Kambing setelah ditambah adalah : "+jumlahKambing);
+    }
+
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+        
+    }
+
+}
